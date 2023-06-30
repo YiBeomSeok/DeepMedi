56f144de23fc108def75330c43ccb83976f3a924
bmsk <vv99911@gmail.com>
2023-06-30 17:03:50 +0900

    Update RemoteDatasource.kt
    - 상수화

--- core/data/src/main/kotlin/org/bmsk/data/datasource/RemoteDatasource.kt
+++ core/data/src/main/kotlin/org/bmsk/data/datasource/RemoteDatasource.kt
@@ -26,23 +26,23 @@ class RemoteDatasource @Inject constructor(
     fun uploadImage(image: Bitmap): Flow<NetworkResult<UploadImageResult>> = flow {
         emit(NetworkResult.Loading)
         val file = image.toFile()
-        val requestFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
-        val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
+        val requestFile = file.asRequestBody(MEDIA_TYPE.toMediaTypeOrNull())
+        val body = MultipartBody.Part.createFormData(IMAGE_KEY, file.name, requestFile)
 
         try {
             val response = deepMediService.uploadImage(body)
             if (response.isSuccessful) {
                 val responseBody = response.body()
-                if(responseBody != null) {
+                if (responseBody != null) {
                     emit(NetworkResult.Success(responseBody.toDomainModel()))
                 } else {
-                    emit(NetworkResult.Error("No response body found"))
+                    emit(NetworkResult.Error(NO_RESPONSE_BODY_FOUND))
                 }
             } else {
-                emit(NetworkResult.Error("Upload failed with HTTP code: ${response.code()}"))
+                emit(NetworkResult.Error(message = UPLOAD_FAILED_HTTP_CODE + response.code()))
             }
         } catch (e: Exception) {
-            emit(NetworkResult.Error("Upload failed with exception: ${e.message}"))
+            emit(NetworkResult.Error(message = UPLOAD_FAILED_EXCEPTION + e.message))
         }
     }.flowOn(ioDispatcher)
 
@@ -52,16 +52,16 @@ class RemoteDatasource @Inject constructor(
             val response = deepMediService.getMeasuredResult()
             if (response.isSuccessful) {
                 val responseBody = response.body()
-                if(responseBody != null) {
+                if (responseBody != null) {
                     emit(NetworkResult.Success(responseBody.toDomainModel()))
                 } else {
-                    emit(NetworkResult.Error("No response body found"))
+                    emit(NetworkResult.Error(message = NO_RESPONSE_BODY_FOUND))
                 }
             } else {
-                emit(NetworkResult.Error("HTTP request failed with code: ${response.code()}"))
+                emit(NetworkResult.Error(message = HTTP_REQUEST_FAILED_CODE + response.code()))
             }
         } catch (e: Exception) {
-            emit(NetworkResult.Error("HTTP request failed with exception: ${e.message}"))
+            emit(NetworkResult.Error(message = HTTP_REQUEST_FAILED_EXCEPTION + e.message))
         }
     }.flowOn(ioDispatcher)
 
@@ -74,5 +74,15 @@ class RemoteDatasource @Inject constructor(
             this.compress(Bitmap.CompressFormat.JPEG, 100, out)
         }
         return file
+    }
+
+    companion object {
+        const val IMAGE_KEY = "file"
+        const val MEDIA_TYPE = "image/jpeg"
+        const val NO_RESPONSE_BODY_FOUND = "No response body found"
+        const val UPLOAD_FAILED_HTTP_CODE = "Upload failed with exception: "
+        const val UPLOAD_FAILED_EXCEPTION = "Upload failed with exception: "
+        const val HTTP_REQUEST_FAILED_CODE = "HTTP request failed with code: "
+        const val HTTP_REQUEST_FAILED_EXCEPTION = "HTTP request failed with exception: "
     }
 }
